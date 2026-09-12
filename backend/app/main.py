794128cb2c1f89fea00b0a7c01c6951cfb45e8fe
mminaamnadeem-tech <mminaamnadeem@gmail.com>
2026-09-12 17:26:53 +0500

    fix: support local browser origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,8 @@ TEMPLATE_DIR = ROOT_DIR / 'templates'
 app = FastAPI(title='Pre-Legal API')
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:5173', 'http://127.0.0.1:5173'],
+    allow_origins=['null'],
+    allow_origin_regex=r'^https?://(localhost|127\.0\.0\.1|0\.0\.0\.0)(:\d+)?$',
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
